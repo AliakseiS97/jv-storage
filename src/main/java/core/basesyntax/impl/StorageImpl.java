95dd268c89644d7c163dc8de2704282b8214a914
AliakseiS97 <aliakseis97@gmail.com>
2025-09-01 15:16:48 +0200

    Storage solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,49 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Node<K, V>[] nodes = new Node[10];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            Node<K, V> current = nodes[i];
+            if (Objects.equals(current.key, key)) {
+                current.value = value;
+                return;
+            }
+        }
+        nodes[size] = new Node(key, value);
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            Node<K, V> current = nodes[i];
+            if (Objects.equals(current.key, key)) {
+                return current.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
